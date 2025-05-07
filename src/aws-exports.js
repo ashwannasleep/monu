f0a871667bbb74ce8ba59f87bb2545a2edbb784c
Ashley Chang <ashleychang@Ashleys-Mini.attlocal.net>
2025-05-07 18:56:29 -0500

    chore: include aws-exports.js for Amplify config

---
+++ src/aws-exports.js
@@ -0,0 +1,31 @@
+
+const awsmobile = {
+    "aws_project_region": "us-east-1",
+    "aws_cognito_identity_pool_id": "us-east-1:6ef7bc2d-8025-4780-a160-3f1963bbcf16",
+    "aws_cognito_region": "us-east-1",
+    "aws_user_pools_id": "us-east-1_XxhbyWnAG",
+    "aws_user_pools_web_client_id": "591b3s8stf4acah672t54phu81",
+    "oauth": {},
+    "aws_cognito_username_attributes": [
+        "EMAIL",
+        "PHONE_NUMBER"
+    ],
+    "aws_cognito_social_providers": [],
+    "aws_cognito_signup_attributes": [
+        "EMAIL"
+    ],
+    "aws_cognito_mfa_configuration": "OFF",
+    "aws_cognito_mfa_types": [
+        "SMS"
+    ],
+    "aws_cognito_password_protection_settings": {
+        "passwordPolicyMinLength": 8,
+        "passwordPolicyCharacters": []
+    },
+    "aws_cognito_verification_mechanisms": [
+        "EMAIL"
+    ]
+};
+
+
+export default awsmobile;
